d675885654eb240945da6daf7408195d13337847
Kikuo Emoto <kemoto@codemonger.io>
2024-01-10 12:31:44 +0900

    feat: support issue numbers
    - Replaces every occurence of "#" + issue number (1 to 8 digits) with
      the link to the corresponding issue. I had no idea how log an issue
      number can be but believe it is long enough.
    
    issue kikuomax/link-commit-hash#1

--- link-commit-hash.py
+++ link-commit-hash.py
@@ -11,12 +11,16 @@ LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 LOGGER.setLevel(logging.DEBUG)
 
-HASH_PATTERN = re.compile(r'#([0-9a-fA-F]{40})\b')
+COMMIT_HASH_PATTERN = re.compile(r'#([0-9a-fA-F]{40})\b')
 
-ESCAPE_PATTERN = re.compile(r'\[#([0-9a-fA-F]{40})\]\(([^)]+)\)')
+# TODO: how long can it be? Up to 8 digits for now.
+ISSUE_NUMBER_PATTERN = re.compile(r'#([1-9][0-9]{0,7})\b')
+
+# includes both commit hashes and issue numbers
+ESCAPE_PATTERN = re.compile(r'\[#([0-9a-fA-F]{1,40})\]\(([^)]+)\)')
 
 RECOVERY_PATTERN = re.compile(
-    r'\[__ESCAPED_COMMIT_HASH_LINK_([0-9a-fA-F]{40})\]\(([^)]+)\)'
+    r'\[__ESCAPED_COMMIT_HASH_LINK_([0-9a-fA-F]{1,40})\]\(([^)]+)\)'
 )
 
 
@@ -26,9 +30,13 @@ def process_markdown(markdown_path, owner, project):
     Replaces every occurence of '#' + full SHA-1 hash (40 characters) with
     a link to the corresponding GitHub commit page.
 
+    Replaces every occurence of '#' + issue number (1-8 digits) with a link
+    to the corresponding GitHub issue page.
+
     Replaced markdown is written to the standard output.
     """
-    substitution = f'[#\\1](https://github.com/{owner}/{project}/commit/\\1)'
+    commit_substitution = f'[#\\1](https://github.com/{owner}/{project}/commit/\\1)'
+    issue_substitution = f'[#\\1](https://github.com/{owner}/{project}/issues/\\1)'
     escape_substitution = f'[__ESCAPED_COMMIT_HASH_LINK_\\1](\\2)'
     recovery_substitution = f'[#\\1](\\2)'
     with open(markdown_path, mode='r', encoding='utf-8') as markdown_in:
@@ -36,7 +44,9 @@ def process_markdown(markdown_path, owner, project):
             # prevents already replaced links from being replaced
             line = re.sub(ESCAPE_PATTERN, escape_substitution, line)
             # replaces commit hashes with links
-            line = re.sub(HASH_PATTERN, substitution, line)
+            line = re.sub(COMMIT_HASH_PATTERN, commit_substitution, line)
+            # replaces issue numbers with links
+            line = re.sub(ISSUE_NUMBER_PATTERN, issue_substitution, line)
             # recovers the escaped links
             line = re.sub(RECOVERY_PATTERN, recovery_substitution, line)
             sys.stdout.write(line)
